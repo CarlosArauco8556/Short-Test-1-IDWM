e70fddb5c89e4e4f10303b3fea6f4055b1b30528
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-11-27 14:50:25 -0300

    feat: create character module.

---
+++ src/app/characters/components/previous-button/previous-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previous-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './previous-button.component.html',
+  styleUrl: './previous-button.component.css'
+})
+export class PreviousButtonComponent {
+
+}
